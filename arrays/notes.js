06ab7d52bd29ed32d7d46cf8c2b16a71ed11155a
Marwen Magri <marwen.magri@gomycode.co>
2020-03-21 14:12:49 +0100

    the intro to arrays methods

---
+++ arrays/notes.js
@@ -0,0 +1,38 @@
+const notes = ["Note 1", " Note 2", "Note 3"]
+
+// from the end of the arrays
+// console.log( notes.pop())
+// notes.push("My new note")
+
+
+
+
+//from the start of the arrays
+
+// console.log(notes.shift())
+// notes.unshift("My first note")
+
+
+
+
+
+//from any position in the array
+
+// notes.splice(1,1, "this is the new second item")
+
+notes[2] = "this is now the new note 3" 
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(notes.length)
+console.log(notes)
+// console.log(notes[notes.length-1])
---
+++ arrays/todo.js
@@ -0,0 +1,31 @@
+const todos = ["Sleep","Eat","Repeat","And sometime you need to play "]
+
+
+
+todos.splice(2,1)
+todos.push('the new last todo')
+todos.shift()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(`you have ${todos.length} todos`)
+
+// console.log(`todo : ${todos[0]} and you need after that to : ${todos[todos.length-2]}`)
+
+
+
+console.log(todos)
