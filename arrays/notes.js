da6906450ff023ca098812832c7e313b8a54776b
Marwen Magri <marwen.magri@gomycode.co>
2020-03-21 14:48:08 +0100

    forEach loop for arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,10 +1,8 @@
-const notes = ["Note 1", " Note 2", "Note 3"]
+const notes = ["Note 1", "Note 2", "Note 3"]
 
 // from the end of the arrays
 // console.log( notes.pop())
 // notes.push("My new note")
-
-
 
 
 //from the start of the arrays
@@ -12,26 +10,18 @@ const notes = ["Note 1", " Note 2", "Note 3"]
 // console.log(notes.shift())
 // notes.unshift("My first note")
 
-
-
-
-
 //from any position in the array
 
 // notes.splice(1,1, "this is the new second item")
 
 notes[2] = "this is now the new note 3" 
 
+//callback function is just a function passed to function
 
-
-
-
-
-
-
-
-
-
+notes.forEach(function(item, index) {
+    console.log(index)
+    console.log(item)
+})
 
 console.log(notes.length)
 console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,31 +1,16 @@
 const todos = ["Sleep","Eat","Repeat","And sometime you need to play "]
 
-
-
 todos.splice(2,1)
 todos.push('the new last todo')
-todos.shift()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+todos.shift( )
 
 console.log(`you have ${todos.length} todos`)
 
 // console.log(`todo : ${todos[0]} and you need after that to : ${todos[todos.length-2]}`)
+// console.log(todos)
 
 
-
-console.log(todos)
+todos.forEach(function(item,index){
+    console.log(`${index+1}. ${item}`)
+ 
+})
