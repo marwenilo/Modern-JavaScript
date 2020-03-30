d3903f4be0fbc7789cc3fa83dc0f60606a260b5f
Marwen Magri <marwen.magri@gomycode.co>
2020-03-30 14:42:04 +0100

    DOM print todos

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,5 +1,14 @@
 //DOM - Document Object Model
-
+const notes = [{
+   title: 'mMy next trip',
+   body: 'I would like to go ti Italie'
+   }, {
+   title: 'Habbits to work on',
+   body: 'Exercise. Eating a bit better'
+   }, {
+   title: 'Office modification',
+   body: 'Get a new seat'
+   }]
 //Query and remove
 // const p = document.querySelector('p')
 // p.remove()
@@ -9,3 +18,8 @@ const ps = document.querySelectorAll('p')
 ps.forEach(p=>{
    p.textContent="*****"
 })
+
+//add a new element
+const newParg = document.createElement('p')
+newParg.textContent = "teesssttt 3"
+document.querySelector('body').appendChild(newParg)
--- todo-app.js/todo-app.js
+++ todo-app.js/todo-app.js
@@ -1,6 +1,40 @@
-const p = document.querySelectorAll('p')
+const todos = [{
+   todo:"Sleep",
+   completed:true
+                   },{
+   todo:"Eat",
+   completed:false
+                   },{
+   todo:"Repeat",
+   completed:true
+                   },{
+   todo:"And sometime you need to play ",
+   completed:false
+                   }]
 
-p.forEach(el=>{
-   let parg = el.textContent.includes('Sleep')
-   parg? el.remove():el
-})
+let count = 0
+const isComplete = todos.filter(el=> !el.completed)
+const summary = document.createElement('h2')
+
+summary.textContent=`You have ${isComplete.length} todos left`
+document.querySelector('body').appendChild(summary)
+                   
+
+
+// const p = document.querySelectorAll('p')
+
+// p.forEach(el=>{
+//    let parg = el.textContent.includes('Sleep')
+//    parg? el.remove():el
+// })
+todos.map(el=> 
+   // el.completed? 
+   (
+  test = document.createElement('p'),
+  test.textContent = el.todo,
+   document.querySelector('body').appendChild(test)
+)
+// :(
+// null
+// )
+)
