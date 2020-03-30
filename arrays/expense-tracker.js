d9c76dae8323db9a2fcab17dc5a5c93b646b782e
Marwen Magri <marwen.magri@gomycode.co>
2020-03-30 12:54:00 +0200

    expense v2

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,32 +1,46 @@
 const account = {
     name: 'Marwen Magri',
     expenses: [],
+    income: [],
     addExpense:function (description,amount){
         this.expenses.push({
             description:description,amount:amount
         })
     },
     getAccountSummary:function () {
-        let total = 0 
-        this.expenses.forEach(obj => {
-            total += obj.amount
+        let totalExpense = 0 
+        let totalIncome = 0
+        let balance = 0
+        this.expenses.forEach(expense => {
+            totalExpense += expense.amount
         });
-       return  `${this.name} has ${total} in expenses.`
-    }
+        
+        this.income.forEach(income => {
+            totalIncome += income.amount
+        });
+         balance = totalIncome - totalExpense
+       return  `${this.name} has $${balance} in balance. $${totalIncome} in income, $${totalExpense} in expense`
+    },
+    addIncome:function (description,amount){
+        this.income.push({
+            description:description,amount:amount
+        })
+    },
 }
 
 // Expense -> description, amount
 //addEspense -> description, amount 
 //getAccountSummary -> total up all expenses -> Marwen Magri has $1250 in expenses. 
 
+//add income array to account
+//addIncome -> description, amount
+//tweak getAccountSummary
+
+
+//Marwen Magri has a balance of $10. $100 in income. $900 in expense
 account.addExpense ('Rent', 950)
 account.addExpense('Coffe',2 )
-account.addExpense ('Rent', 950)
-account.addExpense('Coffe',2 )
-account.addExpense ('Rent', 950)
-account.addExpense('Coffe',2 )
-account.addExpense ('Rent', 950)
-account.addExpense('Coffe',2 )
-account.addExpense ('Rent', 950)
-account.addExpense('Coffe',2 )
+account.addIncome ('Rent', 1000)
+account.addIncome('Coffe',2000 )
+
 console.log(account.getAccountSummary())
