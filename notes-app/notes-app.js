8e1d74f3361ba679bb1d58be49a2b731764009b0
Marwen Magri <marwen.magri@gomycode.co>
2020-03-30 13:56:31 +0100

    DOM expo

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,11 @@
-console.log('testttttttttt')
+//DOM - Document Object Model
+
+//Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+//Query all and remove
+const ps = document.querySelectorAll('p')
+ps.forEach(p=>{
+   p.textContent="*****"
+})
